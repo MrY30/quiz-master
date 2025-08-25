56c994b32675cc691b87fc315a6241782d59113a
MrY30 <jeomamacosschool@gmail.com>
2025-08-25 11:27:37 +0800

    Added Modals

--- js/dark_mode.js
+++ js/dark_mode.js
@@ -1,18 +1,8 @@
 const darkModeButton = document.getElementById('dm-button');
 const body = document.getElementById('main-body');
 
-let value = 1;
-
-darkModeButton.addEventListener('click', toggleDarkMode);
-
-function toggleDarkMode()
-{
-    value *= -1;
-    if (value === -1) { // DARK MODE
-        body.classList.replace('light-theme', 'dark-theme');
-        darkModeButton.classList.replace('bxs-sun-dim', 'bxs-moon');
-    } else {
-        body.classList.replace('dark-theme', 'light-theme');
-        darkModeButton.classList.replace('bxs-moon', 'bxs-sun-dim');
-    }
-}
+darkModeButton.addEventListener('click', ()=>{
+    body.classList.toggle('dark-theme');
+    darkModeButton.classList.toggle('bxs-moon');
+    darkModeButton.classList.toggle('bxs-sun-dim');
+});
